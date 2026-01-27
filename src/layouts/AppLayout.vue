9f7aad4429a11fbb118aa17cdd7c138107acd189
Andres Jhmndz <test432mhz@gmail.com>
2026-01-26 22:03:11 -0400

    My First commit

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Navbar from '@/components/navbar/Navbar.vue';
+
+</script>
+
+<template>
+  <div>
+    <Navbar />
+    <main>
+      <router-view />
+    </main>
+  </div>
+</template>
---
+++ src/modules/home/view/Home.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <app-layout>
+    <div>
+      <h1>About</h1>
+    </div>
+  </app-layout>
+</template>
+
+<style lang="scss" scoped></style>
